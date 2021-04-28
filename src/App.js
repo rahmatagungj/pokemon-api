851f0bbdd2ce590cf9dbe9c0d3dfd8f89e0bfda6
rahmatagungj <rahmatagungj@gmail.com>
2021-04-28 17:16:02 +0700

    add api fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import React from "react";
+import MediaCard from "./Components/Card";
+
+const {useEffect,useState} = React;
 
 function App() {
+  const [datas,setDatas] = useState({})
+  const [pokemons, setPokemons] = useState([])
 
+  useEffect( async () => {
+    await fetch("https://pokeapi.co/api/v2/pokemon?limit=100").then(res => {
+       if (res.status === 200) return res.json();
+    }).then(resJson => {
+      console.log(resJson);
+      setPokemons([{data : resJson.results}])
+      setDatas({...resJson})
+    })
+    return () => {
+      console.log("cleaning up ...")
+    }
+  }, [])
 
   return (
     <div className="App">
-     heello
+      {
+      pokemons.map((r,idx) => {
+        console.log(r.data[idx].name);
+      })}
     </div>
   );
 }
